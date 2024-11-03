ac030a3b1107237763e0155d6ce3d4c1db89f2f8
Dev_Avishka <90317810+Dev-Avishka@users.noreply.github.com>
2024-11-03 08:22:40 +0530

    Update Main.java
    Added a comment detailing itss purpose

--- src/main/java/com/dev/Main.java
+++ src/main/java/com/dev/Main.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+
+    //ignore this, this is just for testing purposes 
     public static void main(String[] args) {
         Controllers.playD4AFile("C:/Users/Devdisa/Desktop/bgm.d4a");
     }
